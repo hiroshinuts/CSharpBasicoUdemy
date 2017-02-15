832015d95bc383c8a6df6dd02e9fbeb409671ec0
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2017-02-14 23:09:54 -0200

    Aprendendo C# - Basico - Udemy

---
+++ exemplo1/exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variaveis
+            string nome = "Neri";
+            string sobrenome = "Neitzke";
+            int idade = 21;
+
+            Console.WriteLine("Video Aulas C# do Prof Neri Neitzke");
+            Console.WriteLine(nome + "" + sobrenome + ", tem" + idade + " anos");
+
+            Console.ReadKey();
+        }
+    }
+}
